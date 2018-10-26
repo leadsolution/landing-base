55f252a021bba05bc31460b8e77c31845941b326
leocavalcante <lc@leocavalcante.com>
2018-10-25 22:31:18 -0300

    React works on Static Exporting!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,28 @@
-export default () => <div>Welcome to next.js!</div>
+import React from "react";
+
+interface Props {
+
+}
+
+interface State {
+  count: number;
+}
+
+export default class Index extends React.Component<Props, State> {
+  state = {
+    count: 0
+  }
+
+  onClick(event: React.MouseEvent<HTMLAnchorElement>) {
+    event.preventDefault();
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  render() {
+    const { count } = this.state
+
+    return (
+      <a href="#" onClick={this.onClick.bind(this)}>{count}</a>
+    );
+  }
+}
