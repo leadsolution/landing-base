44689372585aa8f4b6cb8b625389f117bf17b4a8
leocavalcante <lc@leocavalcante.com>
2018-12-18 18:08:03 -0200

    Add default head, closes #2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import Head from "next/head";
-import Form from "../components/form";
-import ReactGA from "react-ga";
+import React from "react";
 import ReactPixel from "react-facebook-pixel";
-
+import ReactGA from "react-ga";
+import Form from "../components/form";
 import "./index.scss";
 
 export default class Index extends React.Component {
@@ -18,7 +17,11 @@ export default class Index extends React.Component {
   render() {
     return (
       <div>
-        <Head></Head>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <title>Landing_Base</title>
+        </Head>
         <main role="main">
           <Form />
         </main>
