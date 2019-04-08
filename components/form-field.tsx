b682a880fcdc430ff132110f701320f5debf9103
leocavalcante <lc@leocavalcante.com>
2019-04-08 17:51:11 -0300

    Add textarea, closes #19

--- components/form-field.tsx
+++ components/form-field.tsx
@@ -12,7 +12,15 @@ type FormFieldProps = {
   disabled?: boolean;
 };
 
-export default function FormField({name, label, type = "text", required = true, children = [], placeholder = undefined, disabled = false}: FormFieldProps) {
+export default function FormField({
+  name,
+  label,
+  type = "text",
+  required = true,
+  children = [],
+  placeholder = undefined,
+  disabled = false,
+}: FormFieldProps) {
   function input() {
     switch (type) {
       case "tel":
@@ -25,7 +33,7 @@ export default function FormField({name, label, type = "text", required = true, 
             required={required}
             disabled={disabled}
             placeholder={placeholder}
-            options={{phone: true, phoneRegionCode: "br"}}
+            options={{ phone: true, phoneRegionCode: "br" }}
           />
         );
 
@@ -42,14 +50,19 @@ export default function FormField({name, label, type = "text", required = true, 
             options={{
               blocks: [3, 3, 3, 2],
               delimiters: [".", ".", "-"],
-              numericOnly: true
+              numericOnly: true,
             }}
           />
         );
 
       case "select":
         return (
-          <select className="form-control" id={name} name={name} required={required} disabled={disabled}>
+          <select
+            className="form-control"
+            id={name}
+            name={name}
+            required={required}
+            disabled={disabled}>
             <option value="" selected disabled>
               {placeholder === undefined ? "Selecione" : placeholder}
             </option>
@@ -67,7 +80,7 @@ export default function FormField({name, label, type = "text", required = true, 
             required={required}
             disabled={disabled}
             placeholder={placeholder}
-            options={{blocks: [5, 3], delimiter: "-", numericOnly: true}}
+            options={{ blocks: [5, 3], delimiter: "-", numericOnly: true }}
           />
         );
 
@@ -84,14 +97,35 @@ export default function FormField({name, label, type = "text", required = true, 
             options={{
               blocks: [2, 2, 4],
               delimiters: ["/", "/"],
-              numericOnly: true
+              numericOnly: true,
             }}
           />
         );
 
+      case "textarea":
+        return (
+          <textarea
+            className="form-control"
+            id={name}
+            name={name}
+            required={required}
+            disabled={disabled}
+            placeholder={placeholder}
+          />
+        );
+
       default:
-        return <input type={type} className="form-control" id={name} name={name} required={required} disabled={disabled}
-                      placeholder={placeholder}/>;
+        return (
+          <input
+            type={type}
+            className="form-control"
+            id={name}
+            name={name}
+            required={required}
+            disabled={disabled}
+            placeholder={placeholder}
+          />
+        );
     }
   }
 
